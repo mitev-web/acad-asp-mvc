49bf176a1c2691394658cfa4863259d5b9ee1bad
Georgi Mitev <mitev.web@gmail.com>
2011-12-20 18:52:58 +0200

    Signed-off-by: Georgi Mitev <mitev.web@gmail.com> CSS3, JavaScript, HTML5, jQuery

---
+++ Tutorials/MvcMusic/MvcMusic/Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusic.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public string Index()
+        {
+            return "Index() y0";
+        }
+
+
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            return message;
+        }
+
+        //GET: /Store/Details/5
+        public string Details(int id)
+        {
+            string message = "Store.Details, ID = " + id;
+            return message;
+        }
+
+    }
+}
