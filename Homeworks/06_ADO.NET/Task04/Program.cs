3543510edac6765d6dc01393fd4904cecc8a9f0e
Georgi Mitev <mitev.web@gmail.com>
2012-01-05 12:19:38 +0200

    ref

--- Homeworks/06_ADO.NET/Task04/Program.cs
+++ Homeworks/06_ADO.NET/Task04/Program.cs
@@ -1,54 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.Objects;
-using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
 
 namespace ADO.NET
 {
     class Program
     {
-     
-            //Implement previous by using native SQL query and executing it through the 
-            //ObjectContext.
 
-                //SELECT * from Customers c
-                //join orders o
-                //On o.customerid = c.customerid
-                //where YEAR(o.ShippedDate) = 1997
-                //and o.ShipCountry = 'Canada'
-
-        private const string CONNECTION_STRING = "Server=.\\SQLEXPRESS;" +
-            " Database=Northwind; Integrated Security=true";
-        private const string COMMAND_SELECT_CUSTOMERS = @"SELECT * from Customers c
-                                                        join orders o
-                                                        On o.customerid = c.customerid
-                                                        where YEAR(o.ShippedDate) = 1997
-                                                        and o.ShipCountry = 'Canada'";
         static void Main()
         {
-            SqlConnection con = new SqlConnection(CONNECTION_STRING);
-            con.Open();
-            try
-            {
-                SqlCommand command = new SqlCommand(COMMAND_SELECT_CUSTOMERS, con);
-                SqlDataReader reader = command.ExecuteReader();
 
-                using (reader)
-                {
-                    while (reader.Read())
-                    {
-                        string customerName = (String)reader["ContactName"];
-                        Console.WriteLine(customerName);
-                    }
-                }
-            }
-            finally
+
+            //4.Implement previous by using native SQL 
+            //query and executing it through the ObjectContext.
+
+            string queryString =
+            @"SELECT VALUE DISTINCT Customer
+            FROM NorthwindEntities.Customers AS Customer
+            INNER JOIN NorthwindEntities.Orders AS Order
+            ON Customer.CustomerID=Order.CustomerID
+            WHERE Year(Order.OrderDate) == 1997 &&
+            Order.ShipCountry=='Canada'";
+
+            var customerQuery = new ObjectQuery<Customer>(
+            queryString, DAO.db);
+
+            foreach (var q in customerQuery)
             {
-                con.Close();
+                Console.WriteLine("The customer {0} have order made in 1997 and live in Canada.", q.ContactName);
             }
-  
         }
     }
 }
