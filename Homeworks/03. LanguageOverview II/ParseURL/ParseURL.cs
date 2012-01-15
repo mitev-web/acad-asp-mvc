ffc6b131aed20002c432c10a9f3bcfc1c1445164
Georgi Mitev <mitev.web@gmail.com>
2012-01-15 10:35:32 +0200

    ref

---
+++ Homeworks/03. LanguageOverview II/ParseURL/ParseURL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+class ParseURL
+{
+    static void Main(string[] args)
+    {
+        //parse the [protocol]://[server]/[resource]
+
+        string URL = "http://academy.telerik.com/student-courses/seo-course";
+
+        Regex reg = new Regex(@"\b(?<protocol>http://)(?<domain>.*\.[a-zA-Z0-9]{2,14}\.[a-z]{2,4})(?<resource>/.*)\b");
+
+        Match m = reg.Match(URL);
+
+        Group protocol = m.Groups["protocol"];
+        Group domain = m.Groups["domain"];
+        Group resource = m.Groups["resource"];
+
+
+        Console.WriteLine("the protocol is {0}\nthe domain is {1} \nand the resource is {2}\n", protocol, domain, resource);       
+    }
+}
+
