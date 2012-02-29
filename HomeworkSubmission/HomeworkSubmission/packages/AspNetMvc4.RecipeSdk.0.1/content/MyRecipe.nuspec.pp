d8da719cc2a7eee0d86c7541cf79241eb8546d33
Georgi Mitev <mitev.web@gmail.com>
2012-02-29 12:39:16 +0200

    ref

---
+++ HomeworkSubmission/HomeworkSubmission/packages/AspNetMvc4.RecipeSdk.0.1/content/MyRecipe.nuspec.pp
@@ -0,0 +1,16 @@
+<?xml version="1.0"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
+  <metadata>
+    <id>$AssemblyName$</id>
+    <version>1.0</version>
+    <title />
+    <authors>Tell the package author to identify him or herself!</authors>
+    <owners />
+    <requireLicenseAcceptance>false</requireLicenseAcceptance>
+    <description>Tell the package author to write a description please!</description>
+    <summary />
+  </metadata>
+  <files>
+    <file src="bin\Debug\$AssemblyName$.dll" target="recipes\$AssemblyName$.dll" />
+  </files>
+</package>
